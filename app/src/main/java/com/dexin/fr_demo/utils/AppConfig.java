f6d8bbc0d2389cb085ca519b2180c6f6ba3de199
yangdengxueshi <1437977371@qq.com>
2018-04-12 20:23:52 +0800

    从MainActivity开始解析!

--- app/src/main/java/com/dexin/fr_demo/utils/AppConfig.java
+++ app/src/main/java/com/dexin/fr_demo/utils/AppConfig.java
@@ -1,8 +1,14 @@
 package com.dexin.fr_demo.utils;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.support.annotation.Nullable;
 
 import com.orhanobut.logger.Logger;
+
+import java.util.Objects;
 
 
 /**
@@ -17,9 +23,34 @@ public final class AppConfig {
      * @param filePath 文件路径
      * @return Bitmap
      */
+    @Nullable
     public static Bitmap decodeImage(String filePath) {
         try {
+            //"旋转"和"缩放"
+            Matrix lMatrix = new Matrix();//图像矩阵
+            int orientation = new ExifInterface(filePath).getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    lMatrix.postRotate(90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    lMatrix.postRotate(180);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    lMatrix.postRotate(270);
+                    break;
+                default:
+            }
 
+            BitmapFactory.Options lBitmapFactoryOptions = new BitmapFactory.Options();
+            lBitmapFactoryOptions.inSampleSize = 1;
+            lBitmapFactoryOptions.inJustDecodeBounds = false;
+//            lBitmapFactoryOptions.inMutable = true;
+            Bitmap lBitmap = BitmapFactory.decodeFile(filePath, lBitmapFactoryOptions);
+            Bitmap lTempBitmap = Bitmap.createBitmap(lBitmap, 0, 0, lBitmap.getWidth(), lBitmap.getHeight(), lMatrix, true);
+            if (!Objects.equals(lBitmap, lTempBitmap)) lBitmap.recycle();
+            Logger.t(TAG).d("decodeImage: " + "检查目标图像 " + lTempBitmap.getWidth() + "×" + lTempBitmap.getHeight());
+            return lTempBitmap;
         } catch (Exception e) {
             Logger.t(TAG).e(e, "decodeImage: ");
         }
